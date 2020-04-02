b841fe9666be84426ae3db1eaf621f1900a8b851
Renan Tavares <58276037+Renan-Tavares@users.noreply.github.com>
2020-04-02 15:58:39 -0300

    while

---
+++ ambiente.js
@@ -0,0 +1,15 @@
+let c = 1 
+while(c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+}
+let d = 1
+do {
+    console.log(`Passo ${d}`)
+    d++
+} while (d <= 5)
+
+
+
+
+ // console.log('Tudo Bem?')
