0d9720474e650f4c4577d7dba9dab636c35ff0ec
Renan Tavares <58276037+Renan-Tavares@users.noreply.github.com>
2020-04-07 11:42:35 -0300

    Analisador de Números

--- script.js
+++ script.js
@@ -1,19 +1,55 @@
-function Calcular() {
-    
-    let num = document.getElementById('txtnum')
-    let tab = document.getElementById('seltab')
-       if (num.value.length == 0) {
-        window.alert('[ERRO] Digite um valor.')
-    }  else {
-        let n = Number(num.value)
-        let c = 1
-        tab.innerHTML = ''
-        while (c <= 10) {
-            let item = document.createElement('option')
-            item.text = `${n} x ${c} = ${n*c}`
-            tab.appendChild(item)
-            c++
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
+let valores = []
+
+function isNumero(n) {
+    if(Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function inLista(n, l) {
+    if(l.indexOf(Number(n)) != -1) {
+        return true
+    } else { 
+        return false
+    }
+}
+
+function adicionar() {
+    if(isNumero(num.value) && !inLista(num.value, valores)) {
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+        res.innerHTML = ''
+
+    } else {
+        window.alert('Valor inválido ou ja encontrado na lista')
+    }
+    num.value = ''
+    num.focus()  
+}
+    function finalizar() {
+        if(valores.length == 0) {
+            window.alert('Adicione valeres antes de finalizar.')
+        } else {
+            let tot = valores.length
+            let maior = valores[0]
+            let menor = valores[0]
+            for ( let pos in valores) {
+                if(valores[pos] > maior)
+                maior = valores[pos]
+                if (valores[pos] < menor)
+                    menor = valores[pos]
+
+            }
+            res.innerHTML = ''
+            res.innerHTML += `<p> Ao todo, temos ${tot} números cadastrados. </p>`
+            res.innerHTML += `<p> O maior valor é ${maior}.`
+            res.innerHTML += `<p> O menor valor é ${menor}.`
         }
     }
-    
-} 
